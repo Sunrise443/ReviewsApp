9d698375dc2c695accbb5ac916400ee7f27ef60a
Sunrise443 <sunkensun0@gmail.com>
2024-08-19 23:40:34 +0300

    Some changes with constants.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
+
 
 interface HeaderProps {
     text?: string
@@ -9,9 +11,17 @@ export function Header ({
     text,
     rightProfile = "/userbutton.png",
 }: HeaderProps) {
+
+    const [isOpened, setIsOpened] = useState<boolean>(false)
+
+    function openOnClick() {
+        setIsOpened(!isOpened)
+        console.log(isOpened)
+    }
+
     return (
         <div className="flex h-12 justify-between items-center mx-auto bg-dark font-medium">
-            <button className="ml-3"><img src="/menu.png" alt="Menu Button" /></button>
+            <button className="ml-3" onClick={openOnClick}><img src="/menu.png" alt="Menu Button" /></button>
             <div>{text}</div>
             <NavLink to="/user" className="mr-2"><img src={rightProfile} alt="User Button" /></NavLink>
         </div>
--- frontend/src/components/LeftSLider.tsx
+++ frontend/src/components/LeftSLider.tsx
@@ -1,8 +1,11 @@
 import { NavLink } from "react-router-dom"
 
+
+
 export function LeftSlider () {
+
     return (
-        <div className="bg-light w-72 h-screen p-8 text-white" style={{ height: innerHeight - 48 }}>
+        <div className="bg-light w-72 h-screen p-8 text-white" style={{ height: innerHeight - 48 , width: "w-0"}}>
             <ul>
                 <li><NavLink to="/diary">Diary</NavLink></li>
                 <li><NavLink to='/friendsposts'>For Friends</NavLink></li>
