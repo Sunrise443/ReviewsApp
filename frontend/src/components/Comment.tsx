77ee4e47b8fef8bd78e8a597029a69dbc272bd69
Sunrise443 <sunkensun0@gmail.com>
2024-08-21 02:42:27 +0300

    Finally working faster and adding Post and Comments components

---
+++ frontend/src/components/Comment.tsx
@@ -0,0 +1,8 @@
+export function Comment () {
+
+    return (
+        <div className="h-fit bg-darker m-10 p-5 rounded-lg">
+            
+        </div>
+    )
+}
---
+++ frontend/src/components/Comments.tsx
@@ -0,0 +1,10 @@
+import { Comment } from "./Comment"
+
+export function Comments () {
+
+    return (
+        <div className="h-fit bg-dark m-10 p-5 rounded-lg">
+            <Comment/>
+        </div>
+    )
+}
